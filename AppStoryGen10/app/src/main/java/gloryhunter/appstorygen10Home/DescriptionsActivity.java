19ea5aac0207f468b1843b8e56cf5d94453ce061
snow_siechau <chautho95@gmail.com>
2017-09-09 10:20:50 +0700

    update

---
+++ AppStoryGen10/app/src/main/java/gloryhunter/appstorygen10Home/DescriptionsActivity.java
@@ -0,0 +1,16 @@
+package gloryhunter.appstorygen10Home;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DescriptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_descriptions);
+
+
+        getSupportActionBar().hide();
+    }
+}
